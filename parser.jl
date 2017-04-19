9a603518319d370927ec76c3d48d39d725eddeb4
mbfeci <mbfeci@gmail.com>
2017-04-19 14:40:50 +0300

    dicts to dict

--- parser.jl
+++ parser.jl
@@ -1,27 +1,21 @@
-
-
-
-
-function parse_files(caption_files)
-	dicts = Array{Array{Tuple{Int64, Tuple{Int64, Array{String, 1}}},1},1}();
+function parse_file(caption_file)
+	dict = Array{Tuple{Int64, Int64, Array{String, 1}},1}();
 	
-	for i=1:length(caption_files)
-		file = open(caption_files[i])
-		push!(dicts, parse_flickr30k(readlines(file)))
-	end
+	file = open(caption_file)
+	dict = parse_flickr30k(readlines(file))
+	vocab = createVocabulary(dict)
 	
-	vocab = createVocabulary(dicts)
-	return dicts, vocab
+	return dict, vocab
 end
 
 function parse_flickr30k(lines)
-	dict = Array{Tuple{Int64, Tuple{Int64, Array{String, 1}}},1}();  # (id, (#no, Sentence))
+	dict = Array{Tuple{Int64, Int64, Array{String, 1}},1}();  # (id, (#no, Sentence))
 	for i = 1 : length(lines)
 		tokens = map(lowercase, split(lines[i],['.','#','\t',' ', '\n']))
 		filter!(t->length(t)>0,tokens);
-		push!(dict, (parse(Int64, tokens[1]), (parse(Int64, tokens[3]), tokens[4:end])))  
+		push!(dict, (parse(Int64, tokens[1]), length(tokens)-3, tokens[4:end])) 
 	end
-	sort!(dict; by = t->t[1])
+	sort!(dict; by = t->t[2])
 	return dict
 end
 
@@ -33,15 +27,13 @@ function parse_mscoco()
 
 end
 
-function createVocabulary(dicts)
+function createVocabulary(dict)
     vocab = Dict{String,Int}()
 	get!(vocab, "<s>", 1)       #Special start token
 	get!(vocab, "</s>", 2)      #Special end token
-	for dict in dicts
-		for element in dict
-			for word in element[2][2]
-				get!(vocab, word, length(vocab)+1)
-			end
+	for element in dict
+		for word in element[3]
+			get!(vocab, word, length(vocab)+1)
 		end
 	end
     return vocab
