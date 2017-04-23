939305853e255744fe7e57c631e99a34ae20421d
mbfeci <mbfeci@gmail.com>
2017-04-24 02:46:14 +0300

    Eliminating words used less than 5 times

--- parser.jl
+++ parser.jl
@@ -13,7 +13,7 @@ function parse_flickr30k(lines)
 	for i = 1 : length(lines)
 		tokens = map(lowercase, split(lines[i],['.','#','\t',' ', '\n']))
 		filter!(t->length(t)>0, tokens);
-		push!(dict, (parse(Int64, tokens[1]), length(tokens)-3, map(t->strip(t,[' ', '.', ',','#', '\'', ')', '(', '!', '/', '?', '\t', '`']),tokens[4:end]))) 
+		push!(dict, (parse(Int64, tokens[1]), length(tokens)-3, map(t->strip(t,[' ', '.', ',','#', '\'', ')', '(', '!', '/', '?', '\t', '`', '"']),tokens[4:end]))) 
 	end
 	sort!(dict; by = t->t[2])
 	return dict
@@ -29,12 +29,18 @@ end
 
 function createVocabulary(dict)
     vocab = Dict{String,Int}()
+	counter = Dict{String, Int}()
 	get!(vocab, "<s>", 1)       #Special start token
 	get!(vocab, "</s>", 2)      #Special end token
+	get!(vocab, "</u>", 3)		#Special unknown token
 	for element in dict
 		for word in element[3]
-			get!(vocab, word, length(vocab)+1)
+			counter[word] = get(counter, word, 0)+1
 		end
+	end
+	
+	for word in keys(counter)
+		counter[word] > 4 && get!(vocab, word, length(vocab)+1)
 	end
     return vocab
 end
